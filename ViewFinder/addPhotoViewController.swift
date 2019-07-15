b6aa669058426e98a30ed1d7fdf8ecad76a78eab
Apple <apple@Apples-MacBook-Pro-4.local>
2019-07-15 18:33:17 -0500

    iteration 1

---
+++ ViewFinder/addPhotoViewController.swift
@@ -0,0 +1,118 @@
+//
+//  addPhotoViewController.swift
+//  ViewFinder
+//
+//  Created by Apple on 7/15/19.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import UIKit
+
+class addPhotoViewController: UITableViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+
+    var imagePicker = UIImagePickerController()
+   
+ 
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        imagePicker.delegate = self}
+    
+    
+    @IBAction func addPhotoTapped(_ sender: Any) {
+    imagePicker.sourceType = .camera
+        present (imagePicker, animated: true, completion: nil)}
+    
+   @IBAction func takePhotoTapped(_ sender: Any) {
+        imagePicker.sourceType = .camera
+        present (imagePicker, animated:true, completion: nil)}
+    
+    @IBAction func albumTapped(_ sender: Any) {
+        imagePicker.sourceType = .savedPhotosAlbum
+        present (imagePicker, animated: true, completion: nil)}
+    
+    
+    @IBAction func photoLibraryTapped(_ sender: Any) {
+    imagePicker.sourceType = .photoLibrary
+        present (imagePicker, animated: true, completion: nil)}
+    
+    
+    
+    @IBOutlet weak var newImage: UIImageView!
+    
+    internal func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        if let selectedImage =
+        info [UIImagePickerController.InfoKey.originalImage] as? UIImage
+        {newImage.image = selectedImage}
+    }
+    
+  
+
+    
+    //tableview
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
